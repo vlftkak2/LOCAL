ca3805b07b31449c4757079c033d27ab02803ac6
vlftkak2 <vlftkak2@naver.com>
2024-11-01 14:04:18 +0900

    Update globalfunction.py

--- streamlit_app/modules/globalfunction.py
+++ streamlit_app/modules/globalfunction.py
@@ -7,17 +7,7 @@ import time
 import pyodbc
 
 # DB 연결
-#conn = pymssql.connect(host=st.secrets['server'], user=st.secrets['username'], password=st.secrets['password'], database=st.secrets['database'], charset="utf8", autocommit=True)
 conn = pymssql.connect(host='192.168.110.49', user='APR', password='Aa1234!!', database='APR', charset="utf8", autocommit=True)
-# conn_str= (
-#     "DRIVER={ODBC Driver 17 for SQL Server};"
-#     "SERVER=192.168.35.201,1433;"
-#     "DATABASE=Delivery;"
-#     "UID=root;"
-#     "PWD=Aa1234!!;"        
-# )
-# conn = pyodbc.connect(conn_str)
-
 cursor = conn.cursor()
 
 # 재고부족 확인
