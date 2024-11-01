76ff0cf91e1027487166c23b1a48ba2177677772
vlftkak2 <vlftkak2@naver.com>
2024-11-01 13:50:17 +0900

    Update globalfunction.py

--- streamlit_app/modules/globalfunction.py
+++ streamlit_app/modules/globalfunction.py
@@ -8,7 +8,7 @@ import pyodbc
 
 # DB 연결
 #conn = pymssql.connect(host=st.secrets['server'], user=st.secrets['username'], password=st.secrets['password'], database=st.secrets['database'], charset="utf8", autocommit=True)
-conn = pymssql.connect(host='192.168.104.182:1432', user='APR_ROOT', password='Aa1234!!', database='APR', charset="utf8", autocommit=True)
+conn = pymssql.connect(host='192.168.110.49,1433', user='APR', password='Aa1234!!', database='APR', charset="utf8", autocommit=True)
 # conn_str= (
 #     "DRIVER={ODBC Driver 17 for SQL Server};"
 #     "SERVER=192.168.35.201,1433;"
@@ -22,7 +22,7 @@ cursor = conn.cursor()
 
 # 재고부족 확인
 def TEST():
-    fu_query = "SELECT TOP 10 CONVERT(NVARCHAR(50),Country) COUNTRY,CONVERT(NVARCHAR(50),Brand) BRAND,ORDERNUMBER FROM SHOPI01T"
+    fu_query = "SELECT TOP 5 CONVERT(NVARCHAR(50),Manager) Manager,TrackNumber FROM DHL01T "
     data = pd.read_sql_query(fu_query,conn)
     DeliveryData = pd.DataFrame(data)
     return DeliveryData
